80a6f3563ec3a94de460929cf1eef44c2642180e
Thomas Mortagne <thomas.mortagne@gmail.com>
2017-07-23 15:35:32 +0200

    ADd page export

--- src/main/java/org/xwiki/contrib/sourcesync/internal/SourceSyncExporter.java
+++ src/main/java/org/xwiki/contrib/sourcesync/internal/SourceSyncExporter.java
@@ -20,6 +20,10 @@
 package org.xwiki.contrib.sourcesync.internal;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collection;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -27,7 +31,12 @@ import javax.inject.Provider;
 import javax.inject.Singleton;
 
 import org.xwiki.component.annotation.Component;
+import org.xwiki.contrib.sourcesync.SourceSynchIndex;
 import org.xwiki.extension.ExtensionId;
+import org.xwiki.extension.repository.InstalledExtensionRepository;
+import org.xwiki.extension.xar.internal.handler.XarExtensionHandler;
+import org.xwiki.extension.xar.internal.repository.XarInstalledExtension;
+import org.xwiki.extension.xar.internal.repository.XarInstalledExtensionRepository;
 import org.xwiki.filter.FilterEventParameters;
 import org.xwiki.filter.FilterException;
 import org.xwiki.filter.event.model.WikiDocumentFilter;
@@ -36,9 +45,9 @@ import org.xwiki.filter.instance.input.DocumentInstanceInputProperties;
 import org.xwiki.filter.instance.input.EntityEventGenerator;
 import org.xwiki.filter.output.BeanOutputFilterStream;
 import org.xwiki.filter.output.BeanOutputFilterStreamFactory;
+import org.xwiki.filter.output.DefaultOutputStreamOutputTarget;
 import org.xwiki.filter.output.OutputFilterStreamFactory;
-import org.xwiki.filter.output.StringWriterOutputTarget;
-import org.xwiki.filter.output.WriterOutputTarget;
+import org.xwiki.filter.output.OutputStreamOutputTarget;
 import org.xwiki.filter.xar.internal.XARFilter;
 import org.xwiki.filter.xar.internal.XARFilterUtils;
 import org.xwiki.filter.xar.output.XAROutputProperties;
@@ -71,7 +80,19 @@ public class SourceSyncExporter
     private XWikiDocumentFilterUtils filterUtils;
 
     @Inject
+    @Named(XarExtensionHandler.TYPE)
+    private InstalledExtensionRepository extensions;
+
+    @Inject
+    private SourceSynchIndex index;
+
+    @Inject
     private Provider<XWikiContext> xcontextProvider;
+
+    private XarInstalledExtensionRepository getXarInstalledExtensionRepository()
+    {
+        return (XarInstalledExtensionRepository) this.extensions;
+    }
 
     public Job exportExtension(ExtensionId extensionId)
     {
@@ -89,47 +110,66 @@ public class SourceSyncExporter
 
     public void exportDocument(XWikiDocument document) throws FilterException, IOException
     {
-        WriterOutputTarget target = new StringWriterOutputTarget();
+        Collection<XarInstalledExtension> extensions =
+            getXarInstalledExtensionRepository().getXarInstalledExtensions(document.getDocumentReference());
 
-        // Input
-        DocumentInstanceInputProperties documentProperties = new DocumentInstanceInputProperties();
-        documentProperties.setVerbose(false);
-        documentProperties.setWithJRCSRevisions(false);
-        documentProperties.setWithRevisions(false);
-
-        // Output
-        XAROutputProperties xarProperties = new XAROutputProperties();
-        xarProperties.setForceDocument(true);
-        xarProperties.setTarget(target);
-        xarProperties.setVerbose(false);
-        BeanOutputFilterStream<XAROutputProperties> xarFilter =
-            ((BeanOutputFilterStreamFactory<XAROutputProperties>) this.xarOutputFilterStreamFactory)
-                .createOutputFilterStream(xarProperties);
-        XARFilter filter = (XARFilter) xarFilter.getFilter();
-
-        FilterEventParameters documentParameters = null;
-        DocumentReference documentReference = null;
-        documentReference = document.getDocumentReference();
-        for (SpaceReference spaceReference : documentReference.getSpaceReferences()) {
-            filter.beginWikiSpace(spaceReference.getName(), FilterEventParameters.EMPTY);
+        if (extensions.isEmpty()) {
+            return;
         }
 
-        documentParameters = new FilterEventParameters();
-        documentParameters.put(WikiDocumentFilter.PARAMETER_LOCALE, document.getDefaultLocale());
-        filter.beginWikiDocument(documentReference.getName(), documentParameters);
+        XarInstalledExtension extension = extensions.iterator().next();
 
-        // Document Locale events
-        this.documentSerializer.write(document, xarFilter, documentProperties);
+        Path sourcePath = this.index.getSourcePath(extension.getId());
 
-        // Document and spaces events
-        filter.endWikiDocument(documentReference.getName(), documentParameters);
+        OutputStream outputStream = Files.newOutputStream(sourcePath);
+        try (OutputStreamOutputTarget target = new DefaultOutputStreamOutputTarget(outputStream, true)) {
+            // Input
+            DocumentInstanceInputProperties documentProperties = new DocumentInstanceInputProperties();
+            documentProperties.setVerbose(false);
+            documentProperties.setWithJRCSRevisions(false);
+            documentProperties.setWithRevisions(false);
 
-        documentReference = document.getDocumentReference();
-        for (EntityReference reference = documentReference.getParent(); reference instanceof SpaceReference; reference =
-            reference.getParent()) {
-            filter.beginWikiSpace(reference.getName(), FilterEventParameters.EMPTY);
+            // Output
+            XAROutputProperties xarProperties = new XAROutputProperties();
+            xarProperties.setForceDocument(true);
+            xarProperties.setTarget(target);
+            xarProperties.setVerbose(false);
+            BeanOutputFilterStream<XAROutputProperties> xarFilterStream =
+                ((BeanOutputFilterStreamFactory<XAROutputProperties>) this.xarOutputFilterStreamFactory)
+                    .createOutputFilterStream(xarProperties);
+            XARFilter xarFilter = (XARFilter) xarFilterStream.getFilter();
+
+            ///////////////////////////////////////////////
+            // Events
+            ///////////////////////////////////////////////
+
+            // Begin space(s)
+            FilterEventParameters documentParameters = null;
+            DocumentReference documentReference = null;
+            documentReference = document.getDocumentReference();
+            for (SpaceReference spaceReference : documentReference.getSpaceReferences()) {
+                xarFilter.beginWikiSpace(spaceReference.getName(), FilterEventParameters.EMPTY);
+            }
+
+            // Begin document
+            documentParameters = new FilterEventParameters();
+            documentParameters.put(WikiDocumentFilter.PARAMETER_LOCALE, document.getDefaultLocale());
+            xarFilter.beginWikiDocument(documentReference.getName(), documentParameters);
+
+            // Document Locale events
+            this.documentSerializer.write(document, xarFilterStream, documentProperties);
+
+            // End document
+            xarFilter.endWikiDocument(documentReference.getName(), documentParameters);
+
+            // End space(s)
+            documentReference = document.getDocumentReference();
+            for (EntityReference reference =
+                documentReference.getParent(); reference instanceof SpaceReference; reference = reference.getParent()) {
+                xarFilter.beginWikiSpace(reference.getName(), FilterEventParameters.EMPTY);
+            }
+
+            xarFilterStream.close();
         }
-
-        xarFilter.close();
     }
 }
